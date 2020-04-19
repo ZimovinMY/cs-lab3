789b64fffb0fa8b3286295d3accd782fd567dca2
ZimowinMY <misch.zimowin2013@mail.ru>
2020-04-20 01:19:57 +0300

    code:гистограмма как изображение в формате SVG (2 часть)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+void
+svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+void
+svg_end() {
+    cout << "</svg>\n";
+}
+void
+svg_rect(double x, double y, double width, double height,string stroke = "black", string fill = "black"){
+    cout << "<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />";
+}
+void
+
+svg_text(double left, double baseline, string text,size_t bin) {
+    cout << "<text x='"<<left<<"' y='"<<baseline<<"'>"<<bin<<"</text>";
+}
+void
+show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    double top = 0;
+    for (size_t bin : bins) {
+    const double bin_width = BLOCK_WIDTH * bin;
+    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin),bin);
+    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"red");
+    top += BIN_HEIGHT;
+}
+    svg_end();
+}
 vector<double>
 input_numbers(size_t count) {
     vector<double> result(count);
@@ -10,8 +51,7 @@ input_numbers(size_t count) {
     return result;
 }
 void
-find_minmax(vector<double> numbers, double& min, double& max)
-{
+find_minmax(vector<double> numbers, double& min, double& max){
     min = numbers[0];
     max = numbers[0];
     for (double number : numbers)
@@ -27,8 +67,7 @@ find_minmax(vector<double> numbers, double& min, double& max)
     }
 }
 vector <size_t>
-make_histogram(vector <double> numbers,size_t bin_count,double min,double max)
-{
+make_histogram(vector <double> numbers,size_t bin_count,double min,double max){
     vector<size_t> bins(bin_count);
     for (double number : numbers)
     {
@@ -42,8 +81,7 @@ make_histogram(vector <double> numbers,size_t bin_count,double min,double max)
     return(bins);
 }
 void
-show_histogram_text(vector<size_t>bins)
-{
+show_histogram_text(vector<size_t>bins){
     const size_t SCREEN_WIDTH = 80;
     const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
@@ -100,6 +138,6 @@ main() {
     find_minmax(numbers, min, max);
 
     const auto bins = make_histogram(numbers, bin_count,min,max);
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
     return 0;
 }
