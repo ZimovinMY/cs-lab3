174bd50dca29b75a29d98b9da5b7bf26c035e20d
ZimowinMY <misch.zimowin2013@mail.ru>
2020-04-20 18:21:07 +0300

    code:вынес функции, связанные с svg в новый модуль v2.0

--- svgheader.h
+++ svgheader.h
@@ -1,6 +1,8 @@
 #ifndef SVGHEADER_H_INCLUDED
 #define SVGHEADER_H_INCLUDED
 #include <iostream>
+#include <vector>
+using namespace std;
 void svg_begin(double width, double height);
 void svg_end();
 void svg_rect(double x, double y, double width, double height,string stroke, string fill);
