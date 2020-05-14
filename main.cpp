5a2e880ea98f9c8544d94bda696799a13af02951
ZimowinMY <misch.zimowin2013@mail.ru>
2020-05-15 02:02:50 +0300

    code:добавил read_input() новый параметр: bool prompt

--- main.cpp
+++ main.cpp
@@ -14,21 +14,31 @@ input_numbers(istream& in,size_t count) {
 }
 
 Input
-read_input(istream& in) {
+read_input(istream& in,bool prompt) {
     Input data;
-    cerr << "Enter number count: ";
     size_t number_count;
-    cin >> number_count;
+    if(prompt)
+    {
+    cerr << "Enter number count: ";
+    in >> number_count;
     cerr << "Enter numbers: ";
     data.numbers = input_numbers(in, number_count);
     cerr << "Enter column count: ";
-    cin >> data.bin_count;
+    in >> data.bin_count;
+    }
+    else
+    {
+        in >> number_count;
+        data.numbers = input_numbers(in, number_count);
+        in >> data.bin_count;
+    }
+
     return data;
 }
 
 int
 main() {
-    Input data = read_input(cin);
+    const auto data = read_input(cin,true);
     const auto bins = make_histogram(data);
     show_histogram_svg(bins);
     //show_histogram_text(bins);
