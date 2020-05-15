9567104dd3b4ad925d23ce31b3b68170eba029d2
ZimowinMY <misch.zimowin2013@mail.ru>
2020-05-15 16:47:26 +0300

    code:сделал пункт 3.2.8

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <sstream>
+#include <string>
 #include <curl/curl.h>
 #include <iostream>
 #include <vector>
@@ -37,28 +39,39 @@ read_input(istream& in,bool prompt) {
     return data;
 }
 
+Input
+download(const string& address) {
+    stringstream buffer;
+
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    CURL *curl = curl_easy_init();
+    if(curl) {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+        res = curl_easy_perform(curl);
+        if (res)
+        {
+            cout << curl_easy_strerror(res) << endl;
+            exit(1);
+        }
+    }
+   curl_easy_cleanup(curl);
+   return read_input(buffer, false);
+}
+
 int
 main(int argc, char* argv[]) {
-
-    if (argc > 1)
+    Input input;
+   if (argc > 1)
     {
-       CURL *curl = curl_easy_init();
-        if(curl) {
-            CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-            res = curl_easy_perform(curl);
-            if (res)
-            {
-                cerr << curl_easy_strerror(res) << endl;
-                exit(1);
-            }
-            curl_easy_cleanup(curl);
-        }
-        return 0;
+        input = download(argv[1]);
     }
-    curl_global_init(CURL_GLOBAL_ALL);
-    const auto data = read_input(cin,true);
-    const auto bins = make_histogram(data);
+    else
+    {
+        input = read_input(cin, true);
+    }
+    const auto bins = make_histogram(input);
     show_histogram_svg(bins);
     //show_histogram_text(bins);
     return 0;
